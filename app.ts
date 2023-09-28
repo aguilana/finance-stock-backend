c9d55ab0c65e8a5022eb45573fffaa0a0afe680d
Nick Aguila <aguilawebdev@gmail.com>
2023-09-28 08:11:28 -0400

    setting up basics for the backend. now working on database psql

---
+++ app.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response, NextFunction } from 'express';
+const app = express();
+const cors = require('cors');
+import { CorsOptions } from 'cors';
+const morgan = require('morgan');
+require('dotenv').config();
+
+const whiteList = [process.env.LOCAL_URL, 'http://localhost:5173'];
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,PUT,PATCH,HEAD,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/auth', require('./auth'));
+
+app.use('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    title: 'Hello World!',
+    description:
+      'This is the backend for the finance stock app - auth access needed to advanced features',
+    code: 200,
+    data: null,
+    status: 'success',
+    timestamp: Date.now(),
+    version: '1.0.0',
+  });
+});
+// error handling endware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  console.error(err.stack);
+  const errStatus = (err as any).status || 500;
+  res.status(errStatus).send(err.message || 'Internal server error.');
+});
+
+module.exports = app;
